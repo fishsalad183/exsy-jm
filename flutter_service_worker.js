65d14498e747261e6664a11fbee03d85cfffb87d
v <vbogos3@gmail.com>
2025-02-16 23:20:12 +0100

    Fix overlay dismiss and arrow button positions on mobile screens

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,14 +33,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "fbc1fc951a40156d4249cda028c6a46b
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "85cbe157eec02fc4c1deef894cff7f59",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "aa14b1acaef2b733057279540e37d690",
+"flutter_bootstrap.js": "f4e8a3a51c9756cb3848b93f492454f7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "f26a6f10f158c38f461a9620ce40f02d",
 "/": "f26a6f10f158c38f461a9620ce40f02d",
-"main.dart.js": "b65b106c379bd50a26e22cd71562d595",
+"main.dart.js": "8e71d8213e6185da0a00fe4524d34976",
 "manifest.json": "a2a85110419bb75ea97dba131bfa2a3e",
 "version.json": "b8b6c0c7399e981b54b26fba3661d2f3"};
 // The application shell files that are downloaded before a service worker can
