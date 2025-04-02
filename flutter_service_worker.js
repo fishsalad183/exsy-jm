e5cb55d64fd34f539dd7200f8e283b01abf91432
v <vbogos3@gmail.com>
2025-04-02 23:32:59 +0200

    Update pinch-zoom sensitivity

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -50,14 +50,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "64bcc7c2c01203e52e609ba070ec7932
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "85cbe157eec02fc4c1deef894cff7f59",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "139e629201e675b18789c67e491af714",
+"flutter_bootstrap.js": "ee2592ef23c586e9e1f1391be62d4d3e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "f26a6f10f158c38f461a9620ce40f02d",
 "/": "f26a6f10f158c38f461a9620ce40f02d",
-"main.dart.js": "9efd849a4c531c4775e2626f20fd9396",
+"main.dart.js": "d648fea2021c89caec36834bc2dec234",
 "manifest.json": "a2a85110419bb75ea97dba131bfa2a3e",
 "version.json": "b8b6c0c7399e981b54b26fba3661d2f3"};
 // The application shell files that are downloaded before a service worker can
