2dcbc1fe79da93518ab7a79f53a4385de849b8b3
v <vbogos3@gmail.com>
2025-05-06 02:35:44 +0200

    Update home screen, navigation bar, and albums

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3740214376"
+    serviceWorkerVersion: "1239645980"
   }
 });
